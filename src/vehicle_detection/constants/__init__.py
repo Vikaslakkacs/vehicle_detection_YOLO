37864dad2d8242ab5e991fa96c62795b5d056fac
Vikas Lakka <vikaslakkacs@gmail.com>
2023-10-07 23:25:02 +0530

    added yolo file paths

--- src/vehicle_detection/constants/__init__.py
+++ src/vehicle_detection/constants/__init__.py
@@ -3,3 +3,7 @@ from pathlib import Path
 CONFIG_FILE_PATH= Path("configs/config.yaml")
 ## Path for params file which has all the project parameters
 PARAMS_FILE_PATH= Path("params.yaml")
+###Yolo version yaml file
+YOLO_V_5S_FILE_PATH= Path("configs/yolov5s.yaml")
+## Yolo Parameters yaml file
+YOLO_PARAMS_FILE_PATH= Path("configs/yolo_params.yaml")
