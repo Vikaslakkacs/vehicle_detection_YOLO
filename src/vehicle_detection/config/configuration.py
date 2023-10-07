2541821d98514f04f6f3ff189552a2c2a5c196ce
Vikas Lakka <vikaslakkacs@gmail.com>
2023-10-07 23:24:50 +0530

    Added configs based on yolo

--- src/vehicle_detection/config/configuration.py
+++ src/vehicle_detection/config/configuration.py
@@ -1,15 +1,21 @@
 from vehicle_detection.constants import *
 from vehicle_detection.utils.common import read_yaml, create_directories
 from vehicle_detection.entity import DataIngestionConfig
+from vehicle_detection.entity.config_entity import PrepareBaseModelConfig
+from vehicle_detection import logger
 class ConfigurationManager:
     def __init__(self,
                  config_filepath= CONFIG_FILE_PATH,
-                 params_filepath= PARAMS_FILE_PATH):
+                 params_filepath= PARAMS_FILE_PATH,
+                 yolo_v_5s_filepath= YOLO_V_5S_FILE_PATH,
+                 yolo_params_filepath= YOLO_PARAMS_FILE_PATH):
         """
         Creating artifact directories from config.yaml file
         """
         self.config= read_yaml(config_filepath)
         self.params= read_yaml(params_filepath)
+        self.yolo_v_5s= read_yaml(yolo_v_5s_filepath)
+        self.yolo_params= read_yaml(yolo_params_filepath)
         ##Creating Directories
         create_directories([self.config.artifacts_root])
     
@@ -37,4 +43,31 @@ class ConfigurationManager:
             coordinates_target_path= config.coordinates_target_path
         )
         return data_ingestion_config
+
+
+    ###Prepare basemodel Config
+    def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
+        """Get all the details that are required to download yolo version
+
+        Returns:
+            PrepareBaseModelConfig: PrepareBaseModelConfig
+        """
+        config= self.config.prepare_base_model
+        params= self.params
+        logger.info("base model parameters loaded")
+        ##Create root directory for preparebasemodel
+        create_directories([config.root_dir])
+        logger.info(f"Root directory: {config.root_dir} has created")
+
+        prepare_base_model_config= PrepareBaseModelConfig(
+            root_dir= config.root_dir,
+            base_model_link= config.base_model_link,
+            base_model_path= config.base_model_path,
+            params_image_size= params.IMAGE,
+            params_batch= params.BATCH,
+            params_epochs= params.EPOCHS,
+            params_classes= params.NAMES
+            
+        )
+        return prepare_base_model_config
 
--- src/vehicle_detection/entity/config_entity.py
+++ src/vehicle_detection/entity/config_entity.py
@@ -14,3 +14,14 @@ class DataIngestionConfig:
     yolo_conversion_image_size: list
     yolo_voc_bbx: list
     coordinates_target_path: Path
+
+
+@dataclass(frozen=True)
+class PrepareBaseModelConfig:
+    root_dir: Path
+    base_model_link: str
+    base_model_path: Path
+    params_image_size: int
+    params_batch: int
+    params_epochs: int
+    params_classes: list
